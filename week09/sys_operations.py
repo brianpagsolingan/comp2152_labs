3d2fce1c84b536933ff548b57b1b7ff06d8ad3a9
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2025-03-07 13:40:42 -0500

    getting current PID

--- week09/sys_operations.py
+++ week09/sys_operations.py
@@ -5,6 +5,9 @@ print(f"Machine Type:{platform.machine()}\n ")
 print(f"CPU Architechture: {platform.architecture()}\n")
 
 socket.setdefaulttimeout(50)
-print(f"CPU Architecure: {socket.getdefaulttimeout()}\n")
+print(f"Current default timeout: {socket.getdefaulttimeout()}\n")
+
 print(f"OS Type: {os.name}\n")
 print(f"OS Name: {platform.system()}\n")
+
+print(f"Current PID: {os.getpid()}")
