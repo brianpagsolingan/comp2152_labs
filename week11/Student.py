aa2f2c15366794e9dd146d84d6ecdb4177bb0fb8
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2025-03-21 13:36:28 -0400

    bug fix

--- week11/Student.py
+++ week11/Student.py
@@ -3,7 +3,7 @@ from Person import Person
 class Student(Person):
     def __init__(self, p_name, p_age, p_height, p_major):
         super().__init__(p_name, p_age, p_height)
-        self.__major = p_major
+        self.major = p_major
         print("This time it's a student object")
 
 
